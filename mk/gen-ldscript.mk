561194916de737378cc3b91a7b2f221149b7d069
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-05-04 02:51:47 +0700

    Meson: ld generator fix

--- mk/gen-ldscript.mk
+++ mk/gen-ldscript.mk
@@ -1,6 +1,6 @@
 # Usage:
-# $ make -f mk/generate-ldscript.mk generate DEVICE=stm32f103x8 OPENCM3_DIR=.
-# $ make -f mk/generate-ldscript.mk clean DEVICE=stm32f103x8 OPENCM3_DIR=.
+# $ make -f mk/generate-ldscript.mk generate DEVICE=stm32f103x8 OPENCM3_DIR=. RENAME_TO=somename.ld
+# $ make -f mk/generate-ldscript.mk clean RENAME_TO=somename.ld
 
 include $(OPENCM3_DIR)/mk/genlink-config.mk
 include $(OPENCM3_DIR)/mk/gcc-config.mk
@@ -10,4 +10,4 @@ generate: $(LDSCRIPT)
 	mv $(LDSCRIPT) $(RENAME_TO)
 
 clean:
-	$(Q)$(RM) -rf generated.ld
+	$(Q)$(RM) $(RENAME_TO)
